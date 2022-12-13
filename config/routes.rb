c465bd4d4021c3521c85b20cd43ea384f319f08b
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-12-13 00:03:20 -0500

    added more controllers and concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :posts
-  resources :notifications
-  resources :comments
-authenticate :user, lambda { |u| u.admin? } do
-  mount Sidekiq::Web => '/sidekiq'
-  namespace :madmin do
-    resources :impersonates do
-      post :impersonate, on: :member
-      post :stop_impersonating, on: :collection
-    end
+  resources :posts do
+    resources :comments, module: :posts
   end
-end
+
+  resources :comments do
+    resources :comments, module: :comments
+  end
+
+  get '/privacy', to: 'home#privacy'
+  get '/terms', to: 'home#terms'
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
