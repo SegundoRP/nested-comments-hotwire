7f1deda9a17ffe68d74c504274aed182c4f555c7
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-12-12 23:40:27 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :omniauthable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+  has_person_name
 
+  has_many :comments
+  has_many :notifications, as: :recipient
+  has_many :services
 end
