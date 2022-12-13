7f66bbb86af25c7d19e72766773eee3a1f26cc45
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-12-13 00:23:38 -0500

    run migrations

--- db/migrate/20221213051518_create_notifications.rb
+++ db/migrate/20221213051518_create_notifications.rb
@@ -1,6 +1,10 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
+      t.belongs_to :recipient, polymorphic: true, null: false
+      t.string :type
+      t.text :params
+      t.datetime :read_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_13_043438) do
+ActiveRecord::Schema.define(version: 2022_12_13_051555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
